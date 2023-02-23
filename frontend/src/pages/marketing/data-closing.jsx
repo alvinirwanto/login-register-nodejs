842f9e6e2d9d884721d75efaf2f5300145b800e9
Febe Putrianne <putrifebe@gmail.com>
2023-02-23 14:42:59 +0700

    gittest

--- frontend/src/pages/marketing/data-closing.jsx
+++ frontend/src/pages/marketing/data-closing.jsx
@@ -7,15 +7,9 @@ const DataClosing = () => {
             pageTitle='Data Closing'
         >
             <div className='pt-[10rem]'>
-                {/* <iframe
-                    title="Data Closing 200123 - Dashboard"
-                    // width="600" height="373.5" 
-                    className='w-full h-screen'
-                    src="https://app.powerbi.com/view?r=eyJrIjoiOTUzNzlmNGUtNTRhYy00MzEwLWE5OWUtM2UxY2VhZTdlMGRhIiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe> */}
-
                 <iframe title="Report Section"
                     className='w-full h-screen'
-                    src="https://app.powerbi.com/view?r=eyJrIjoiZmQ1YWJkNWQtODU3YS00MTQ1LWE5OGQtYWM3NmZlYTExMGVlIiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+                    src="https://app.powerbi.com/view?r=eyJrIjoiZWFjMmFjYTItY2MxOS00YmY2LWJjZDAtYjUzNWMxMmM0ZGVkIiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D&pageName=ReportSection2b3fefe0ec91aa4553da" frameborder="0" allowFullScreen="true"></iframe>
             </div>
         </Layout>
     )
