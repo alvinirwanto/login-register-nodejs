ba68b78ab927bae4b6d750224c53a7d350ba88ba
alvinirwanto <irwantoalvin@gmail.com>
2023-02-28 18:06:08 +0700

    fix the presensi iframe

--- frontend/src/pages/pegawai/presensi.jsx
+++ frontend/src/pages/pegawai/presensi.jsx
@@ -11,7 +11,7 @@ const Presensi = () => {
                     title="Data Closing 230223"
                     className='w-full h-screen'
                     // width="600" height="373.5" 
-                    src="https://app.powerbi.com/view?r=eyJrIjoiZWFjMmFjYTItY2MxOS00YmY2LWJjZDAtYjUzNWMxMmM0ZGVkIiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D&pageName=ReportSection2b3fefe0ec91aa4553da" frameborder="0" allowFullScreen="true"></iframe>
+                    src="https://app.powerbi.com/view?r=eyJrIjoiOWJkZmQ2ZmUtMWJhOC00Nzc1LTkwNTgtMjU0ZDk5MWRmMmFjIiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D&pageName=ReportSection0ca669535a15e9d83ea2" frameborder="0" allowFullScreen="true"></iframe>
             </div>
         </Layout>
     )
