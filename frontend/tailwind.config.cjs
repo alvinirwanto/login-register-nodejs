27006df9d0b15199eef2a9359f160689fdac7f5d
alvinirwanto <irwantoalvin@gmail.com>
2023-03-01 14:35:33 +0700

    edit tailwind config

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -6,11 +6,28 @@ module.exports = {
     ],
     theme: {
         extend: {
+            screens: {
+                sm: '640px',
+                // => @media (min-width: 640px) { ... }
+
+                md: '768px',
+                // => @media (min-width: 768px) { ... }
+
+                lg: '1024px',
+                // => @media (min-width: 1024px) { ... }
+
+                xl: '1280px',
+                // => @media (min-width: 1280px) { ... }
+
+                '2xl': '1536px',
+                // => @media (min-width: 1536px) { ... }
+            },
+            
             colors: {
                 'primary-blue': '#00326f',
                 'secondary-blue': '#038abe',
                 'primary-green': '#8eb362',
-                'shadow-modal' : '#0000005a'
+                'shadow-modal': '#0000005a'
             }
         },
     },
