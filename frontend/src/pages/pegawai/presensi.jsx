0e0e470cf277b84458de39a73d453d361094b267
Febe Putrianne <putrifebe@gmail.com>
2023-08-29 09:56:00 +0700

    data pegawai j fungsional and presensi update 290823

--- frontend/src/pages/pegawai/presensi.jsx
+++ frontend/src/pages/pegawai/presensi.jsx
@@ -11,9 +11,7 @@ const Presensi = () => {
                     title="Presensi"
                     className='w-full h-screen'
                     // width="600" height="373.5" 
-                    src="https://app.powerbi.com/view?r=eyJrIjoiNTk1NmU5MjMtZjc2OC00MzRlLWFlZWQtZGRlNjc3Yzg5NDg5IiwidCI6ImYzMTEzNmJjLWE3OTAtNDUwZi04Y2NiLWNhZDNmYWFlNDRmNyIsImMiOjEwfQ%3D%3D" 
-                    frameborder="0"
-                    allowFullScreen="true"></iframe>
+                    src="https://app.powerbi.com/view?r=eyJrIjoiNTk1NmU5MjMtZjc2OC00MzRlLWFlZWQtZGRlNjc3Yzg5NDg5IiwidCI6ImYzMTEzNmJjLWE3OTAtNDUwZi04Y2NiLWNhZDNmYWFlNDRmNyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
             </div>
         </Layout>
     )
