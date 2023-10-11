ecc7af8b5735f7b12fb9a354ebd31b971a8d4b9f
Febe Putrianne <putrifebe@gmail.com>
2023-10-11 09:56:16 +0700

    akreditasi update 111023

--- frontend/src/pages/akreditasi/akreditasi.jsx
+++ frontend/src/pages/akreditasi/akreditasi.jsx
@@ -11,7 +11,7 @@ const Akreditasi = () => {
                     title="Report Section"
                     // width="600" height="486" 
                     className='w-full h-screen'
-                    src="https://app.powerbi.com/view?r=eyJrIjoiYmZiODIzZTAtMjc1NS00MjZiLTk1NmYtYjBkZGJiNGI1ZDcxIiwidCI6ImYzMTEzNmJjLWE3OTAtNDUwZi04Y2NiLWNhZDNmYWFlNDRmNyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+                    src="https://app.powerbi.com/view?r=eyJrIjoiN2Y5NmRlNjYtMjk2MC00MDE1LWFhZTctMTRlZWZkNjExOTAyIiwidCI6ImYzMTEzNmJjLWE3OTAtNDUwZi04Y2NiLWNhZDNmYWFlNDRmNyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
 
             </div>
         </Layout>
